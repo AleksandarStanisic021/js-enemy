0dfb8ac7c83bb20782eff2eb3bd911a76439212e
ciandre1976 <ciandre23@gmail.com>
2026-03-05 14:47:22 +0100

    setup

--- src/main.js
+++ src/main.js
@@ -1,3 +1,13 @@
 import "./style.css";
+import ghostEnemy from "./enemy_ghost.png";
+import spiderEnemy from "./enemy_spider.png";
+import wormEnemy from "./enemy_worm.png";
+
+let ghostImage = new Image();
+ghostImage.src = ghostEnemy;
+let spiderImage = new Image();
+spiderImage.src = spiderEnemy;
+let wormImage = new Image();
+wormImage.src = wormEnemy;
 
 console.log("yaya");
